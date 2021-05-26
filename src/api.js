c0e36d1354b8a2b53046c657b02a605e3f13c50b
starhn87 <starhn87@gmail.com>
2021-05-26 21:49:31 +0900

    solve search issue

--- src/api.js
+++ src/api.js
@@ -23,7 +23,7 @@ export const moviesApi = {
     search: term =>
         api.get("search/movie", {
             params: {
-                query: encodeURIComponent(term)
+                query: term
             }
         })
 }
@@ -41,7 +41,7 @@ export const tvApi = {
     search: term =>
         api.get("search/tv", {
             params: {
-                query: encodeURIComponent(term)
+                query: term
             }
         })
 }
