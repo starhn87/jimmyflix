d20f5600d57d32cc2bc5f619287447ece575d027
starhn87 <starhn87@gmail.com>
2021-05-29 21:18:37 +0900

    make component for search

--- src/Routes/Search.js
+++ src/Routes/Search.js
@@ -1,12 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import Loader from "../Components/Loader";
-import Section from "../Components/Section";
-import Message from "../Components/Message";
-import Poster from "../Components/Poster";
 import Helmet from "react-helmet";
 import { useSearch } from "../hooks/useSearch";
 import { useSearchFunctions, useSearchState, useSearchTerm } from "../contexts/SearchContext";
+import SearchResult from "../Components/SearchResult";
 
 const Container = styled.div`
     padding: 0 20px;
@@ -25,7 +23,7 @@ const Input = styled.input`
 
 function Search() {
     useSearch();
-    const { movieResults, tvResults, error, loading } = useSearchState();
+    const { loading } = useSearchState();
     const { handleSubmit, updateTerm } = useSearchFunctions();
     const searchTerm = useSearchTerm();
 
@@ -40,45 +38,7 @@ function Search() {
         {loading ? (
             <Loader />
         ) : (
-            <>
-                {movieResults && movieResults.length > 0 && (
-                    <Section title="Movie Results">
-                        {movieResults.map(movie =>
-                            <Poster
-                                key={movie.id}
-                                id={movie.id}
-                                imageUrl={movie.poster_path}
-                                title={movie.original_title}
-                                rating={movie.vote_average}
-                                year={movie.release_date && movie.release_date.substring(0, 4)}
-                                isMovie={true}
-                            />
-                        )
-                        }
-                    </Section>
-                )}
-                {tvResults && tvResults.length > 0 && (
-                    <Section title="TV Shows Results">
-                        {tvResults.map(show =>
-                            <Poster
-                                key={show.id}
-                                id={show.id}
-                                imageUrl={show.poster_path}
-                                title={show.original_name}
-                                rating={show.vote_average}
-                                year={show.first_air_date && show.first_air_date.substring(0, 4)}
-                            />
-                        )
-                        }
-                    </Section>
-                )}
-                {error && <Message color="#e74c3c" text={error} />}
-                {tvResults
-                    && movieResults
-                    && tvResults.length === 0
-                    && movieResults.length === 0
-                    && <Message text="Nothing found" color="grey" />}
-            </>
+            <SearchResult />
         )}
     </Container>
 }
